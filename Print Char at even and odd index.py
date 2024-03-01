1d0b41325d7283f21716177d5d99ab90bf628474
DharshVsgn <110477705+dharsh443@users.noreply.github.com>
2024-03-01 12:13:57 +0530

    Add files via upload

---
+++ Print Char at even and odd index.py
@@ -0,0 +1,11 @@
+S=input("enter the String:")
+i=0
+print("Char at even index:")
+while(i<len(S)):
+    print(S[i])
+    i=i+2
+i=1
+print("Char at Odd index:")
+while(i<len(S)):
+    print(S[i])
+    i=i+2
